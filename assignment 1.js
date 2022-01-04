79e63e1a7b298667079994382e3b9b9092ede43f
Quazi Yadgar Husain <72057700+quaziyadgar@users.noreply.github.com>
2022-01-04 19:40:40 +0530

    Add files via upload

---
+++ assignment 1.js
@@ -0,0 +1,10 @@
+var a=parseInt(prompt("Enter a number : "));
+if (a>10){
+   alert(a+" is greater than 10");
+}
+else if(a==10){
+    alert(a+" is equal to 10");
+}
+else{
+    alert(a+" is less than 10");
+}
---
+++ assignment 2.js
@@ -0,0 +1,12 @@
+var a=parseInt(prompt("Enter First Number :"));
+var b=parseInt(prompt("Enter Second Number :"));
+var c=parseInt(prompt("Enter Third Number :"));
+if (a>b && a>c){
+    alert(a+" is greatest number");
+}
+else if (b>a && b>c){
+    alert(b+" is greatest number");
+}
+else{
+    alert(c+" is greatest number");
+}
